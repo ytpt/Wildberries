055a37a2707f58c9a80f9aa1a29b5e6b48e43187
ytpt <88822646+ytpt@users.noreply.github.com>
2022-02-24 02:41:38 +0700

    The first step

---
+++ cart.js
@@ -0,0 +1,15 @@
+const cart = function() {
+    const cartBtn = document.querySelector('.button-cart');
+    const cart = document.getElementById('modal-cart');
+    const closeBtn = cart.querySelector('.modal-close');
+
+    cartBtn.addEventListener('click', function() {
+        cart.style.display = 'flex';
+    })
+
+    closeBtn.addEventListener('click', function() {
+        cart.style.display = '';
+    })
+}
+
+cart(); 
